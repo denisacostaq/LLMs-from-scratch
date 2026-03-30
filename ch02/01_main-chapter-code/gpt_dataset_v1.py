9bc1ffad26413ff33f6e6d6ff827d742d0f047fe
Alvaro Denis <denisacostaq@gmail.com>
2026-03-30 20:39:35 +0300

    add gpt dataset v1

---
+++ ch02/01_main-chapter-code/gpt_dataset_v1.py
@@ -0,0 +1,23 @@
+from torch.utils.data import Dataset
+
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_id = token_ids[i:i + max_length]
+            target_id = token_ids[i + 1:i + max_length + 1]
+
+            self.input_ids.append(input_id)
+            self.target_ids.append(target_id)
+
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
