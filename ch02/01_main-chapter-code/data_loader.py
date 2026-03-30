0d26eaa6942d9ff723171d76f713b533932d6e95
Alvaro Denis <denisacostaq@gmail.com>
2026-03-30 20:46:00 +0300

    test data loader

--- ch02/01_main-chapter-code/data_loader.py
+++ ch02/01_main-chapter-code/data_loader.py
@@ -9,3 +9,11 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, drop_last=drop_last)
     return dataloader
+
+with open("the-verdict.txt", "r", encoding="utf-8") as file:
+    raw_text = file.read()
+
+dataloader = create_dataloader_v1(txt=raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+data_iterator = iter(dataloader)
+first_batch = next(data_iterator)
+print(first_batch)
