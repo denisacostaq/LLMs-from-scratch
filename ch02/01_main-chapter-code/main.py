ca61e78f3b9b89dc3937e1d838d1697d922b0c32
Alvaro Denis <denisacostaq@gmail.com>
2026-03-30 20:13:47 +0300

    ilustrative tokenization with regex splits

--- ch02/01_main-chapter-code/main.py
+++ ch02/01_main-chapter-code/main.py
@@ -1,4 +1,8 @@
+import re
+
 with open("the-verdict.txt", "r", encoding="utf-8") as file:
     raw_text = file.read()
 print("Total number of character:", len(raw_text))
 print(raw_text[:99])
+split_text = re.split(r'(\s)', raw_text)
+print(split_text)
