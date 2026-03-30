4d79f088b2e1bf9c10d17176e2de125b126cb94a
Alvaro Denis <denisacostaq@gmail.com>
2026-03-30 20:15:38 +0300

    tokeni based in individual words and punctuation characters

--- ch02/01_main-chapter-code/main.py
+++ ch02/01_main-chapter-code/main.py
@@ -4,6 +4,6 @@ with open("the-verdict.txt", "r", encoding="utf-8") as file:
     raw_text = file.read()
 print("Total number of character:", len(raw_text))
 print(raw_text[:99])
-split_text = re.split(r'(\s)', raw_text)
+split_text = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 striped_tokens = [item for item in split_text if item.strip()]
 print(striped_tokens)
