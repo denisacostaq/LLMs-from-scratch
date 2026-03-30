b5dc5fcfc152c6f7fdb45b2ca853068b46c34e6d
Alvaro Denis <denisacostaq@gmail.com>
2026-03-30 20:19:37 +0300

    mapping tokens to token id

--- ch02/01_main-chapter-code/main.py
+++ ch02/01_main-chapter-code/main.py
@@ -7,3 +7,11 @@ print(raw_text[:99])
 split_text = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 striped_tokens = [item for item in split_text if item.strip()]
 print(striped_tokens)
+
+all_words = sorted(set(striped_tokens))
+vocab_size = len(all_words)
+print("Vocabulary size:", vocab_size)
+
+vocab = {word: index for index, word in enumerate(all_words)}
+for i, item in enumerate(all_words[:50]):
+    print(f"{item}: {i:3d}")
